efb15f249294415f281415911685a11f4dcbd82c
Bruno Marthe <brunomarthe@hotmail.com>
2022-07-28 15:39:46 -0300

    Pix.sln e PayloadService.Test

--- PayloadService.Test/PayloadServiceTest.cs
+++ PayloadService.Test/PayloadServiceTest.cs
@@ -21,11 +21,5 @@ namespace Prime.UnitTests.Services
             // Console.WriteLine(payload);
             Assert.Equals(payload, "00020126450014br.gov.bcb.pix0123brunomarthe72@gmail.com52040000530398654031005802BR5912Bruno Marthe6011Sorocaba SP620705031236304AF4C");
         }
-
-        [Test]
-        public void New_Fail_Pix_Payload(){
-            var payload = _payloadService.GetPayload();
-            Assert.AreNotEqual(payload, Assert.Equals(payload, "00020126450014br.gov.bcb.pix0123brunomarthe72@gmail.com52040000530398654031005802BR5912Bruno Marthe6011Sorocaba SP620705031236304AF4C"));
-        }
     }
 }
